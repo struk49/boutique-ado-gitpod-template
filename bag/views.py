44bafeb4627d170cf8f37f7f0a236d901361c559
struk49 <andrewstruk@yahoo.co.uk>
2023-10-17 10:28:54 +0000

    added adjust bag and remove item views and calc subtotal template filter

--- bag/views.py
+++ bag/views.py
@@ -1,11 +1,4 @@
-from django.shortcuts import render, redirect
-
-# Create your views here.
-
-def view_bag(request):
-    """ a view to return the bag contents page """
-
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, reverse, HttpResponse
 
 # Create your views here.
 
@@ -41,34 +34,51 @@ def add_to_bag(request, item_id):
     request.session['bag'] = bag
     return redirect(redirect_url)
     
-    return render(request, 'bag/bag.html')
 
-def add_to_bag(request, item_id):
-    """ add quatity of the specified product to the shopping bag """
+def adjust_bag(request, item_id):
+    """Adjust the quantity of the specified product to the specified amount"""
 
     quantity = int(request.POST.get('quantity'))
-    redirect_url = request.POST.get('redirect_url')
     size = None
     if 'product_size' in request.POST:
         size = request.POST['product_size']
     bag = request.session.get('bag', {})
 
     if size:
-        if item_id in list(bag.keys()):
-            if size in bag[item_id]['items_by_size'].keys():
-                bag[item_id]['items_by_size'][size] += quantity
-            else:
-                bag[item_id]['items_by_size'][size] = quantity
+        if quantity > 0:
+            bag[item_id]['items_by_size'][size] = quantity
         else:
-            bag[item_id] = {'items_by_size': {size: quantity}}
+            del bag[item_id]['items_by_size'][size]
+            if not bag[item_id]['items_by_size']:
+                bag.pop(item_id)
     else:
-        if item_id in list(bag.keys()):
-            bag[item_id] += quantity
-        else:
+        if quantity > 0:
             bag[item_id] = quantity
-        bag[item_id] = quantity
-    
-    request.session['bag'] = bag
-    print(request.session['bag'])
-    return redirect(redirect_url)
+        else:
+            bag.pop(item_id)
 
+    request.session['bag'] = bag
+    return redirect(reverse('view_bag'))
+
+
+def remove_from_bag(request, item_id):
+    """Remove the item from the shopping bag"""
+
+    try:
+        size = None
+        if 'product_size' in request.POST:
+            size = request.POST['product_size']
+        bag = request.session.get('bag', {})
+
+        if size:
+            del bag[item_id]['items_by_size'][size]
+            if not bag[item_id]['items_by_size']:
+                bag.pop(item_id)
+        else:
+            bag.pop(item_id)
+
+        request.session['bag'] = bag
+        return HttpResponse(status=200)
+
+    except Exception as e:
+        return HttpResponse(status=500)
