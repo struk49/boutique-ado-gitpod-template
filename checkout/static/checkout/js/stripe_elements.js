cd8f89e9a4d4a56d096182a380b4d5b4726bd7de
struk49 <andrewstruk@yahoo.co.uk>
2023-10-22 10:49:03 +0000

    added basic checkout functionality

--- checkout/static/checkout/js/stripe_elements.js
+++ checkout/static/checkout/js/stripe_elements.js
@@ -1,5 +1,5 @@
-var stripe_public_key = $('#id_stripe_public_key').text().slice(1, -1);
-var client_secret = $('#id_client_secret').text().slice(1, -1);
+var stripePublicKey = $('#id_stripe_public_key').text().slice(1, -1);
+var clientSecret = $('#id_client_secret').text().slice(1, -1);
 var stripe = Stripe(stripe_public_key);
 var elements = stripe.elements();
 
@@ -38,4 +38,36 @@ card.addEventListener('change', function (event){
     }else {
         errorDiv.textContent = '';
     }
+});
+
+
+// Handle form submit
+var form = document.getElementById('payment-form');
+
+form.addEventListener('submit', function(ev) {
+    ev.preventDefault();
+    card.update({ 'disabled': true});
+    $('submit-button').attr('disabled', true);
+stripe.confirmCardPayment(clientSecret,{
+        payment_method: {
+            card: card
+        }
+    }).then(function(result) {
+        if (result.error) {
+            var errorDiv = document.getElementById('card-errors');
+        var html = `
+        <span class="icon" role="alert">
+            <i class="fas fa-times"></i>
+        </span>
+        <span >${result.error.message}</span>
+    `;
+    $(errorDiv).html(html);
+    card.update({ 'disabled': false});
+    $('submit-button').attr('disabled', false);
+    }else {
+        if (result.paymentIntent.status === 'Succeeded') {
+         form.submit();
+        }
+    }
+});
 });
